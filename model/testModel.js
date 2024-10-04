e2276878bb42c12ccae88e2da91414e263a5cacd
usman zafar <usmanzafar2003@gmail.com>
2024-10-04 22:32:13 +0500

    added schema for questions in teacher panel

--- model/testModel.js
+++ model/testModel.js
@@ -1,23 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
 const questionSchema = new mongoose.Schema({
-  question: {
-    type: String,
-    required: true,
-  },
-  options: [{ type: String, required: true }],
-  solution: {
-    type: number,
-    required: true,
-  },
+    question: { type: String, required: true },
+    options: [
+        {
+            option: { type: String, enum: ['A', 'B', 'C', 'D'], required: true },
+            text: { type: String, required: true }
+        }
+    ],
+    correctOption: { type: String, enum: ['A', 'B', 'C', 'D'], required: true },
+    teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher', required: false }
 });
 
-const testModel = new mongoose.Schema({
-  teacherId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Teacher",
-  },
-  testName: {
-    type: String,
-  },
-  questions: [questionSchema],
-});
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = Question;
