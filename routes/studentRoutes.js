50d831cd0fda982a80c58de67ed23f081bdab68e
usman zafar <usmanzafar2003@gmail.com>
2024-10-20 20:55:18 +0500

    implemented routes for student

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/home', (req, res) => {
+    res.render('student/home'); // Render the home.ejs (dashboard)
+});
+
+router.get('/solveExam', (req, res) => {
+    res.render('student/solveExam');
+});
+router.get('/mcq', (req, res) => {
+    res.render('mcq');
+});
+router.get('/editProfile', (req, res) => {
+    res.render('student/editProfile');
+});
+
 // Route for student registration
 router.get('/register', (req, res) => {
     res.render('student/register'); // Ensure this points to the correct view
